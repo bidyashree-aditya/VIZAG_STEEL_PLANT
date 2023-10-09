629b6b78e4b3b0c0991fa4bbfdffab0630bb2183
Bidyashree Aditya <111597339+bidyashree-aditya@users.noreply.github.com>
2023-10-09 08:55:42 +0530

    Update Machine.java

--- Machine.java
+++ Machine.java
@@ -1,37 +1,43 @@
 package factory_managment;
-import java.sql.SQLOutput;
 import java.util.Scanner;
-public class Factory implements utility{
-    String Factory_name;
-    String Factory_address;
-    int total_employees;
-    int total_machine_in_stock = 0;
-    String manager_name;
+
+public class Machine extends Factory implements utility{
+    String machine_name;
+    String machine_color;
+    String machine_fuel_type;
+    int machine_price;
+    String machine_type;
+    String machine_transmission;
 
     @Override
-    public void get_details() {
-        System.out.println("Factory Name: "+Factory_name);
-        System.out.println("Factory Address: "+Factory_address);
-        System.out.println("Factory Name: "+Factory_name);
-        System.out.println("Total Employees: "+total_employees);
-        System.out.println("Total Machine In Stock: "+total_machine_in_stock);
-
+    public void get_details(){
+        System.out.println("NAME: "+machine_name);
+        System.out.println("COLOR: "+machine_color);
+        System.out.println("FUEL TYPE: "+machine_fuel_type);
+        System.out.println("PRICE: "+machine_price);
+        System.out.println("MACHINE TYPE: "+machine_type);
+        System.out.println("TRANSMISSION: "+machine_transmission);
     }
+
     @Override
     public void set_details(){
         Scanner sc = new Scanner(System.in);
-        System.out.println("======================= *** ENTER FACTORY DETAILS *** =======================");
+        System.out.println("======================= *** ENTER MACHINE DETAILS *** =======================");
         System.out.println();
-        System.out.print("FACTORY NAME: ");
-        Factory_name = sc.nextLine();
-        System.out.print(("FACTORY ADDRESS: "));
-        Factory_address = sc.nextLine();
-        System.out.print("MANAGER NAME: ");
-        manager_name = sc.nextLine();
-        System.out.print("TOTAL NO OF EMPLOYEES: ");
-        total_employees = sc.nextInt();
-        System.out.print("TOTAL MACHINE IN STOCK: ");
-        total_machine_in_stock = sc.nextInt();
-
+        System.out.print("MACHINE NAME: ");
+        machine_name = sc.nextLine();
+        System.out.print(("MACHINE COLOR: "));
+        machine_color = sc.nextLine();
+        System.out.print("MACHINE FUEL TYPE(ELECTRIC/CARBON): ");
+        machine_fuel_type = sc.nextLine();
+        System.out.print("MACHINE PRICE: ");
+        machine_price = sc.nextInt();
+        sc.nextLine();
+        System.out.print("MACHINE TYPE(AUTOMATIC/MANUAL): ");
+        machine_type = sc.nextLine();
+        System.out.print("TRANSMISSION TYPE(AUTOMATIC/MANUAL): ");
+        machine_transmission = sc.nextLine();
+        total_machine_in_stock++;
     }
 }
+
